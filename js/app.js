b437bd1ae4c13533e7d3a6822c81dd52422a9083
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-02-08 17:14:35 +0200

    creat about me page and add some qusetion

---
+++ js/app.js
@@ -0,0 +1,69 @@
+// 'use strict'
+answer="";
+answer2="";
+answer3="";
+answer4="";
+answer5="";
+
+    answer =prompt('is burger mohammad  FAVORITE FOOD, Y OR N ?').toUpperCase();
+    if(answer =="Y"){
+        answer="correct"
+    }
+    else{
+        answer="wrong answer"
+
+    }
+
+
+    answer2 =prompt(' Do Mohammad like chocolate milkshakes?, Y OR N ?').toUpperCase();
+    if(answer2 =="Y"){
+        answer2="correct";
+    }
+    else{
+        answer2="wrong answer";
+    }
+
+
+    answer3 =prompt('Do Mohammad like travel?, Y OR N ?').toUpperCase();
+    if(answer3 =="Y"){
+        answer3=" correct ";
+
+    }
+    else{
+        answer3="wrong answer"
+    }
+
+    answer4 =prompt('Can Mohammad make hemself disappear?, Y OR N ?').toUpperCase();
+    if(answer4 =="Y"){
+        answer4="wrong answer"
+
+    }
+    else{
+        answer4="correct"
+    }
+
+
+    answer5 =prompt('Do Mohammad like games?, Y OR N ?').toUpperCase();
+    if(answer5 =="Y"){
+        answer5="correct"
+
+    }
+    else{
+        answer5="wrong answer"
+    }
+
+
+alert(`Q1  : ${answer} \nQ2  : ${answer2} \nQ3  : ${answer3} \nQ4  : ${answer4} \nQ5  : ${answer5} \n`);
+
+// Do Mohammad like chocolate milkshakes?
+// Do Mohammad like N?
+// Do Mohammad like travel?
+// Do Mohammad like photography?
+// Do Mohammad like games?
+
+
+
+// Can Mohammad make hemself disappear?
+// Have Mohammad ever hit one of his friends?
+// Do Mohammad know how to play at least one musical instrument?
+// is Mohammad doing what he always wanted to do in his life?
