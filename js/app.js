3730fbe52cafa10678b69bd5b1b25e4ae812fb94
MOHAMMAD ALMOMANI <78731070+M7madMomani2@users.noreply.github.com>
2021-02-10 07:45:24 +0200

    add 2Q to the user

--- js/app.js
+++ js/app.js
@@ -4,14 +4,13 @@ let answer2="";
 let answer3="";
 let answer4="";
 let answer5="";
+let answer6="";
+let answer7="";
+let guess1="96";
+let guess2[3]={GOT , BlindSpot, Supernatural };
 let name1 ="";
 name1 =prompt('what\'s your name ?').toUpperCase();
  
-
-
-
-
-
 answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
     if(answer =="Y"){
         console.log("Q1 correct");
@@ -68,6 +67,30 @@ answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
         console.log("Q5 wrong answer");
         answer5="wrong answer"
     }
+
+let count6=0;
+answer6 =prompt('guess mohammad BD?, Y OR N ?').toUpperCase();
+for (let i = 0 ; i <3 ; i++ ){
+  if (answer6 === guess1){
+   break; 
+ 
+  }
+	}
+
+let count7=0;
+answer7 =prompt('guess one of mohammad favorit film BD?, Y OR N ?').toUpperCase();
+for (let i = 0 ; i <6 ; i++ ){
+ for (let x = 0 ; x< guess2.length(); x++){
+  if (answer7 === guess2[x]){
+   break; 
+  }	
+  else {
+  }
+	}	
+}
+
+alert(`wrong answer \nNumber of attempts ${} `);
+
 
 alert(`Hello  ${name1} \n\n\nQ1  : ${answer} \nQ2  : ${answer2} \nQ3  : ${answer3} \nQ4  : ${answer4} \nQ5  : ${answer5} \n`);
 
