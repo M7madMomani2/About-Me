14f81a98c6c0bebcc09a12ebe8bddd116e953acf
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-02-11 19:30:12 +0200

    fix js problem

--- js/app.js
+++ js/app.js
@@ -6,14 +6,17 @@ let answer4="";
 let answer5="";
 let answer6="";
 let answer7="";
-let guess1="96";
-var guess2=["GOT" , "BlindSpot", "Supernatural" ];
+let guess1="1996";
+let guess2=["GOT" , "BlindSpot", "Supernatural" ];
 let name1 ="";
 let count=0;
+let flage1 = false;
+let count7=0;
+let count6=0;
 name1 =prompt('what\'s your name ?').toUpperCase();
  
 answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
-    if(answer =="Y"){
+    if(answer.toUpperCase() ==="Y"|| answer.toUpperCase() === "YES"){
         console.log("Q1 correct");
         answer="correct";
         count++;
@@ -25,7 +28,7 @@ answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
 
 
     answer2 =prompt(' Does Mohammad like chocolate milkshakes?, Y OR N ?').toUpperCase();
-    if(answer2 =="Y"){
+    if(answer2.toUpperCase() ==="Y"|| answer2.toUpperCase() === "YES"){
         console.log("Q2 correct");
         answer2="correct";
         count++;
@@ -37,7 +40,7 @@ answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
 
 
     answer3 =prompt('Does Mohammad like travel?, Y OR N ?').toUpperCase();
-    if(answer3 =="Y"){
+    if(answer3.toUpperCase() ==="Y"|| answer3.toUpperCase() === "YES"){
         console.log("Q3 correct");
         answer3=" correct ";
         count++;
@@ -48,19 +51,20 @@ answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
     }
 
     answer4 =prompt('Can Mohammad make hemself disappear?, Y OR N ?').toUpperCase();
-    if(answer4 =="Y"){        
+    if(answer4.toUpperCase() ==="Y"|| answer4.toUpperCase() === "YES"){        
         console.log("Q4 wrong answer");
         answer4="wrong answer"
-        count++;
+
     }
     else{        
         console.log("Q4 correct");
         answer4="correct"
+        count++;
     }
 
 
     answer5 =prompt('Do Mohammad like games?, Y OR N ?').toUpperCase();
-    if(answer5 =="Y"){
+    if(answer5.toUpperCase() ==="Y"|| answer5.toUpperCase() === "YES"){
         console.log("Q5 correct");
         answer5="correct"
         count++;
@@ -70,46 +74,34 @@ answer =prompt('Does Mohammad like  coffee, Y OR N ?').toUpperCase();
         answer5="wrong answer"
     }
 
+    answer6 =prompt('guess mohammad BD? \nCorrect answer 1996').toUpperCase();
+    for (let i = 1 ; i <=3 ; i++ ){
+      if (answer6 === guess1){
+        count++;
+        break;
+      }
+      else{
+        alert(`wrong answer \nNumber of attempts ${i-count6} of 3`);
+        answer6 =prompt('guess mohammad BD?').toUpperCase();
+    
+    }
 
+    }
+    answer7 =prompt('guess one of mohammad favorit series : Correct answer :\nGOT\nBlindSpot\nSupernatural').toUpperCase();
+    for (let w = 0 ; w <=6 ; w++ ){
+        for (let k = 0 ; k< guess2.length; k++){
+            if (answer7.toUpperCase() === guess2[k].toUpperCase()){
+                count++;
+                flage1 = true;
+                break;
+            }	
+        }	
 
-
-let count6=0;
-answer6 =prompt('guess mohammad BD?').toUpperCase();
-for (let i = 0 ; i <3 ; i++ ){
-  if (answer6 === guess1){
-    count++;
-    break; 
-  }
-  else{
-    alert(`wrong answer \nNumber of attempts ${i-count6} `);
-    answer6 =prompt('guess mohammad BD?').toUpperCase();
-  
-}
-}
-
-
-
-
-let count7=0;
-answer7 =prompt('guess one of mohammad favorit film').toUpperCase();
-for (let i = 0 ; i <6 ; i++ ){
- for (let x = 0 ; x< guess2.length(); x++){
-  if (answer7 === guess2[x]){
-    count++;
-    break; 
-  }	
-  else {
-    alert(`wrong answer \nNumber of attempts ${i-count7} `);
-    answer7 =prompt('guess one of mohammad favorit film').toUpperCase();
-
-  }
- }	
-}
-alert(`hello`);
-
-
-
-alert(`Hello  ${name1} \n\n\nQ1  : ${answer} \nQ2  : ${answer2} \nQ3  : ${answer3} \nQ4  : ${answer4} \nQ5  : ${answer5} \n`);
-alert(`correct answer =   ${count} \n\nwronge answer  : ${7-count} \n`);
-
-000
+        if (!flage1){
+            alert(`wrong answer \nNumber of attempts ${w-count7} `);
+            answer7 =prompt('guess one of mohammad favorit film').toUpperCase();
+        }
+        else{break;}
+    }
+    alert(`Hello  ${name1} \n\n\nQ1  : ${answer} \nQ2  : ${answer2} \nQ3  : ${answer3} \nQ4  : ${answer4} \nQ5  : ${answer5} \nQ6  : ${answer6} \nQ7  : ${answer7} \n`);
+    alert(`correct answer =   ${count} OF 7  \n`);
